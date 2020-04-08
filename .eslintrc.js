20c31b50d091a64d2bb9b924f4aea0c35bc3ab82
Rafa Bernal <rbernalber@MacBook-Pro-de-Rafa.local>
2020-04-08 13:13:55 +0200

    Install Jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,30 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "es6": true,
-    "node": true
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
   },
-  "globals": {
-    "Atomics": "readonly",
-    "SharedArrayBuffer": "readonly"
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
-  "parser": "@typescript-eslint/parser",
-  "extends": ["plugin:@typescript-eslint/recommended"],
-  "parserOptions": {
-    "ecmaVersion": 2018,
-    "sourceType": "module",
-    "ecmaFeatures": {
-      "jsx": true
-    }
-  },
-  "plugins": [
-    "@typescript-eslint"
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+    'prettier',
   ],
-  "rules": {
-    "semi": ["error", "always"],
-    "quotes": ["error", "double"]
-  }
-};
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['prettier', '@typescript-eslint'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+}
