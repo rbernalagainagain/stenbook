50de77f464fc0f03a63772eaf264f7cdf744dbe0
Rafa Bernal <rbernalber@MacBook-Pro-de-Rafa.local>
2020-04-09 13:31:17 +0200

    Clean Code and project

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,23 @@
 module.exports = {
   stories: ['../stories/*.stories.tsx'],
+  addons: ['@storybook/addon-knobs'],
+
   // webpackFinal: async (config) => {
   //   config.module.rules.push({
   //     test: /\.(ts|tsx)$/,
   //     loader: require.resolve('babel-loader'),
+  //   })
+  //   config.resolve.extensions.push('.ts', '.tsx')
+  //   return config
+  // },
+  // webpackFinal: async (config) => {
+  //   config.module.rules.push({
+  //     test: /\.(ts|tsx)$/,
+  //     use: [
+  //       {
+  //         loader: require.resolve('awesome-typescript-loader'),
+  //       },
+  //     ],
   //   })
   //   config.resolve.extensions.push('.ts', '.tsx')
   //   return config
