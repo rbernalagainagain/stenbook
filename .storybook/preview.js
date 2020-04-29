f2be64b491af1967f8d05f8c3aa17078649a2959
Rafa Bernal <immrb@MacBook-Pro-de-Rafa.local>
2020-04-29 23:37:39 +0200

    Add Addons Center & Update versions

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,6 @@
+import { addDecorator } from '@storybook/html'
 import { defineCustomElements } from '../dist/esm/loader'
+import centered from '@storybook/addon-centered/html'
 
+addDecorator(centered)
 defineCustomElements()
